ab0da15777ae74d702df8b2bdf3c26c0829a04f6
Muhammadamin <fahriddinovmuhammadamin34@gmail.com>
2024-07-18 14:47:21 +0500

    Initial commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const UserRandomNumber = alert("Sizda hozir random sonlar chiqadi");
 const RandomNumber = Math.round(Math.random() * 50);
-alert(Random raqam: ${RandomNumber})
-const register = confirm(Biz ${RandomNumber} sonini 2 kopaytirmohchimiz va bolmohchimiz)
+alert(`Random raqam: ${RandomNumber}`)
+const register = confirm(`Biz ${RandomNumber} sonini 2 kopaytirmohchimiz va bolmohchimiz`)
 
 alert(
     `Random orqali chiqqan son ${RandomNumber} edi
